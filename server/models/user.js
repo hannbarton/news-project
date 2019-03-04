67d4505991f0e610980c0661fe7f1b7162964e4a
Hannah Barton <hannbarton@gmail.com>
2019-03-04 14:48:16 -0500

    adding in sequelize and user model

---
+++ server/models/user.js
@@ -0,0 +1,15 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const User = db.define('user', {
+    email: {
+      type: Sequelize.STRING,
+      unique: true,
+      allowNull: false
+    },
+    password: {
+      type: Sequelize.STRING,
+    }
+})
+
+module.exports = User
