a93766f38d104d7985332fa95bb33ac68c54ebd2
Hannah Barton <hannbarton@gmail.com>
2019-03-04 18:08:10 -0500

    added rest of articles onto setstate

---
+++ client/components/EachArticle.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default (EachArticle = () => {
+	return <div>{"second hello"}</div>;
+});
--- client/components/Home.js
+++ client/components/Home.js
@@ -5,7 +5,11 @@ class Home extends React.Component {
     constructor() {
         super()
         this.state = {
-            buzzfeed: []
+            buzzfeed: [],
+            reddit: [],
+            google: [],
+            natgeo: [],
+            newsci: []
         }
     }
 
@@ -15,7 +19,23 @@ class Home extends React.Component {
 
     async getData() {
         axios.get('/api/articles/buzzfeed')
-        .then(res => this.setState({ posts: res.data }))
+        .then(res => this.setState({ buzzfeed: res.data }))
+        .catch(err => console.log(err))
+
+        axios.get('/api/articles/reddit')
+        .then(res => this.setState({ reddit: res.data }))
+        .catch(err => console.log(err))
+
+        axios.get('/api/articles/google')
+        .then(res => this.setState({ google: res.data }))
+        .catch(err => console.log(err))
+
+        axios.get('/api/articles/natgeo')
+        .then(res => this.setState({ natgeo: res.data }))
+        .catch(err => console.log(err))
+
+        axios.get('/api/articles/newsci')
+        .then(res => this.setState({ newsci: res.data }))
         .catch(err => console.log(err))
     }
 
