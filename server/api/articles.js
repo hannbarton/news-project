b683fab1add132f51fbab72d8374230168d07dff
Hannah Barton <hannbarton@gmail.com>
2019-03-04 16:56:11 -0500

    getting all 5 sources and sending to /api/articles/source

--- server/api/articles.js
+++ server/api/articles.js
@@ -3,14 +3,69 @@ const { Article } = require("../db/models");
 const NewsAPI = require("newsapi");
 const newsapi = new NewsAPI("a1819176962141cdbb40972e986690ae");
 
-router.get("/", async (req, res, next) => {
+router.get("/buzzfeed", async (req, res, next) => {
 	try {
 		newsapi.v2
 			.topHeadlines({
                 sources: "buzzfeed"
 			})
 			.then(response => {
-                console.log("response", response);
+                res.send(response)
+			});
+	} catch (err) {
+		next(err);
+	}
+});
+
+router.get("/reddit", async (req, res, next) => {
+	try {
+		newsapi.v2
+			.topHeadlines({
+                sources: "reddit-r-all"
+			})
+			.then(response => {
+                res.send(response)
+			});
+	} catch (err) {
+		next(err);
+	}
+});
+
+router.get("/google", async (req, res, next) => {
+	try {
+		newsapi.v2
+			.topHeadlines({
+                sources: "google-news"
+			})
+			.then(response => {
+                res.send(response)
+			});
+	} catch (err) {
+		next(err);
+	}
+});
+
+router.get("/natgeo", async (req, res, next) => {
+	try {
+		newsapi.v2
+			.topHeadlines({
+                sources: "national-geographic"
+			})
+			.then(response => {
+                res.send(response)
+			});
+	} catch (err) {
+		next(err);
+	}
+});
+
+router.get("/newsci", async (req, res, next) => {
+	try {
+		newsapi.v2
+			.topHeadlines({
+                sources: "new-scientist"
+			})
+			.then(response => {
                 res.send(response)
 			});
 	} catch (err) {
