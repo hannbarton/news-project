e8c6f29e8a0173dba86291caeda590fd677eafee
Hannah Barton <hannbarton@gmail.com>
2019-03-05 09:49:51 -0500

    added change and submit handlers to form

--- client/components/CreateAccount.js
+++ client/components/CreateAccount.js
@@ -1,49 +1,76 @@
-import React from 'react'
+import React from "react";
+import axios from "axios";
 
 class CreateAccount extends React.Component {
-    constructor() {
-        super()
-        this.state = {
-            firstName: '',
-            lastName: '',
-            email: '',
-            password: ''
-        }
-        this.handleChange = this.handleChange.bind(this)
-        this.handleSubmit = this.handleSubmit.bind(this)
-    }
+	constructor() {
+		super();
+		this.state = {
+			firstName: "",
+			lastName: "",
+			email: "",
+			password: ""
+		};
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
 
-    handleChange(event) {
-        event.preventDefault();
+	handleChange(event) {
+		event.preventDefault();
 		this.setState({
-			[event.target.name]: event.target.value,
-		})
-    }
+			[event.target.name]: event.target.value
+		});
+	}
 
-    handleSubmit() {
-        event.preventDefault();
-        axios.post('/users', {user})
-        .then(res => {
-            console.log(res.data);
-          })
-    }
+	handleSubmit() {
+		event.preventDefault();
+		axios.post("/users", this.state).then(res => {
+			console.log(res.data);
+		});
+	}
 
-    render() {
-        return(
-            <form>
-            <h3>Create an Account</h3>
-            <input type="text" ref="firstName" placeholder="enter your first name" />
-            <br/>
-            <input type="text" ref="lastName" placeholder="enter your last name" />
-            <br/>
-            <input type="text" ref="email" placeholder="enter your email" />
-            <br/>
-            <input type="password" ref="password" placeholder="create password" />
-            <br/>
-            <input type="submit" value="Create Account" onSubmit={this.handleSubmit}/>
-          </form>
-        )
-    }
+	render() {
+		return (
+			<form onSubmit={this.handleSubmit}>
+				<h3>Create an Account</h3>
+				<input
+					type="text"
+					name="firstName"
+					onChange={this.handleChange}
+					value={this.state.name}
+					placeholder="enter your first name"
+				/>
+				<br />
+				<input
+					type="text"
+					name="lastName"
+					onChange={this.handleChange}
+					value={this.state.name}
+					placeholder="enter your last name"
+				/>
+				<br />
+				<input
+					type="text"
+					name="email"
+					onChange={this.handleChange}
+					value={this.state.name}
+					placeholder="enter your email"
+				/>
+				<br />
+				<input
+					type="password"
+					name="password"
+					onChange={this.handleChange}
+					value={this.state.name}
+					placeholder="create password"
+				/>
+				<br />
+				<input
+					type="submit"
+					value="Create Account"
+				/>
+			</form>
+		);
+	}
 }
 
 export default CreateAccount;
