dd7884844b7eea1629f2485feccb5b44ad2d0f98
Hannah Barton <hannbarton@gmail.com>
2019-03-06 17:23:42 -0500

    added star png

--- client/components/Saved.js
+++ client/components/Saved.js
@@ -1,35 +1,51 @@
-import React from 'react'
+import React from "react";
 
 class Saved extends React.Component {
-    constructor() {
-        super()
+	constructor() {
+		super();
 
-        this.state = {
-            userId: null,
-            articles: []
-        }
-    }
+		this.state = {
+			userId: null,
+			articles: []
+		};
+	}
 
-    async componentDidMount() {
+	async componentDidMount() {
+		await axios
+			.get("/api/users/me")
+			.then(res =>
+				this.setState({
+					userId: +res.data.id
+				})
+			)
+			.catch(err => console.log(err));
 
-        await axios.get('/api/users/me')
-		.then(res => this.setState({
-			userId: +res.data.id,
-		}))
-		.catch(err => console.log(err));
+		axios
+			.get(`/api/users/all/${this.state.userId}`)
+			.then(res =>
+				this.setState({
+					articles: res.data.articles
+				})
+			)
+			.catch(err => console.log(err));
+	}
 
-        axios.get(`/api/users/all/${this.state.userId}`)
-
-    }
-
-
-    render() {
-        return (
-            <div className='saved-container'>
-                {'hell0 world'}
-            </div>
-        )
-    }
+	render() {
+		return (
+			<div className="saved-container">
+				{this.state.articles.map(each => {
+					return (
+						<div>
+							<a href={each.url}>
+								{` ${each.title}`}
+								<br />
+							</a>
+						</div>
+					);
+				})}
+			</div>
+		);
+	}
 }
 
 export default Saved;
