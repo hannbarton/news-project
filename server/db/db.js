32d4f842aca908beeeb623c2d90c5add8eff4e89
Hannah Barton <hannbarton@gmail.com>
2019-03-07 09:24:29 -0500

    changed db

--- server/db/db.js
+++ server/db/db.js
@@ -2,8 +2,7 @@ const Sequelize = require('sequelize')
 
 const databaseName = 'news-project'
 
-const db = new Sequelize(
-    `postgres://localhost:5432/${databaseName}`,
+const db = new Sequelize(process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
     {
       logging: false
     }
