2ef602853e8e5a41dfa35b6c309fbc5c4566e737
Hannah Barton <hannbarton@gmail.com>
2019-03-06 22:23:41 -0500

    added disabled button + back in

--- client/components/EachArticle.js
+++ client/components/EachArticle.js
@@ -7,26 +7,32 @@ class EachArticle extends React.Component {
 	constructor(props) {
 		super(props)
 
+		this.state = {
+			flag: false
+		}
+
 		this.handleAddArticle = this.handleAddArticle.bind(this);
 	}
 
 	handleAddArticle() {
-
 		const article = {
 			title: this.props.title,
 			url: this.props.url,
 			source: this.props.source,
 			urlToImage: this.props.urlToImage,
-			userId: this.state.userId
+			userId: this.props.userId
 		}
 		this.props.postArticle(article)
+
+		this.setState({flag: true})
 	}
 
 	render() {
+
 		const {url, title} = this.props;
 		return (
 			<div>
-			<button type='submit' value={title} name="title" onClick={this.handleAddArticle} className='add-button'>+</button>
+			<button type='submit' disabled={this.state.flag} onClick={this.handleAddArticle} className='add-button'>+</button>
 			<a href={url}>
 			{` ${title}`}
 				<br />
